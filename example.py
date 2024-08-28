b10bed8618295a89688d0f8f07587f52f015b7b9
SmaamX <90418723+SmaamX@users.noreply.github.com>
2024-08-28 15:40:46 +0000

    Add files via upload

--- example.py
+++ example.py
@@ -26,8 +26,8 @@ def matr_move():
   cuscol(lis=[str(exit_+item), '\u001b[32;1m', str(exit_), '\u001b[35;1m', str(item), '\u001b[33;1m', str(wall), '\u001b[37m',str(rate), '\u001b[30;1m', str(null), '\u001b[34m'], fp2=0)
   test_list = [[0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 1, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0]]
   item_list = [[0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,3, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0]]
-  exit_list = [[4, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0]]
-  test_list_back = [[0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 2, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 2, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 2, 0, 0, 0, 0 ,0, 0]]
+  exit_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 4, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0]]
+  test_list_back = [[0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 2, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 2, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 0, 0, 0, 0, 0 ,0, 0], [0, 0, 2, 0, 0, 0, 0 ,0, 0]]
   prinx2(d_list(d_list(d_list(test_list, test_list_back), item_list), exit_list), refs=True)
   def refre():
     prinx2(d_list(d_list(d_list(test_list, test_list_back), item_list), exit_list), refs=True)
@@ -37,9 +37,8 @@ def matr_move():
         if check_vi.check_r(item_list, test_list_back, item, wall, False):
           move_2list(test_list, rate, 1)
           prinx2(d_list(d_list(d_list(move_2list(item_list, item, 1), test_list_back),test_list),exit_list))
-          if check_vi.check_r(item_list, exit_list, item, exit_, True):
-            move_2list(test_list, rate, 1)
-            prinx2(d_list(d_list(d_list(move_2list(item_list, item, 1), test_list_back),test_list),exit_list))
+          if check_vi.check_r(item_list, exit_list, item, exit_, True, r=0):
+            prinx2(d_list(d_list(d_list(item_list, test_list_back),test_list),exit_list))
             print("You w0n");exit()
       else:
         prinx2(d_list(d_list(d_list(move_2list(test_list, rate, 1), test_list_back), item_list), exit_list), refs=True)
@@ -49,9 +48,8 @@ def matr_move():
         if check_vi.check_u(item_list, test_list_back, item, wall, False):
           move_2list(test_list, rate, 2)
           prinx2(d_list(d_list(d_list(move_2list(item_list, item, 2), test_list_back),test_list),exit_list))
-          if check_vi.check_u(item_list, exit_list, item, exit_, True):
-            move_2list(test_list, rate, 2)
-            prinx2(d_list(d_list(d_list(move_2list(item_list, item, 2), test_list_back),test_list),exit_list))
+          if check_vi.check_u(item_list, exit_list, item, exit_, True, r=0):
+            prinx2(d_list(d_list(d_list(item_list, test_list_back),test_list),exit_list))
             print("You w0n");exit()
       else:
         prinx2(d_list(d_list(d_list(move_2list(test_list, rate, 2), test_list_back), item_list), exit_list), refs=True)
@@ -61,9 +59,8 @@ def matr_move():
         if check_vi.check_d(item_list, test_list_back, item, wall, False):
           move_2list(test_list, rate, 4)
           prinx2(d_list(d_list(d_list(move_2list(item_list, item, 4), test_list_back),test_list),exit_list))
-          if check_vi.check_d(item_list, exit_list, item, exit_, True):
-            move_2list(test_list, rate, 4)
-            prinx2(d_list(d_list(d_list(move_2list(item_list, item, 4), test_list_back),test_list),exit_list))
+          if check_vi.check_d(item_list, exit_list, item, exit_, True, r=0):
+            prinx2(d_list(d_list(d_list(item_list, test_list_back),test_list),exit_list))
             print("You w0n");exit()
       else:
         prinx2(d_list(d_list(d_list(move_2list(test_list, rate, 4), test_list_back), item_list), exit_list), refs=True)
@@ -73,9 +70,8 @@ def matr_move():
         if check_vi.check_l(item_list, test_list_back, item, wall, False):
           move_2list(test_list, rate, 3)
           prinx2(d_list(d_list(d_list(move_2list(item_list, item, 3), test_list_back),test_list),exit_list))
-          if check_vi.check_l(item_list, exit_list, item, exit_, True):
-            move_2list(test_list, rate, 3)
-            prinx2(d_list(d_list(d_list(move_2list(item_list, item, 3), test_list_back),test_list),exit_list))
+          if check_vi.check_l(item_list, exit_list, item, exit_, True, r=0):
+            prinx2(d_list(d_list(d_list(item_list, test_list_back),test_list),exit_list))
             print("You w0n");exit()
       else:
         prinx2(d_list(d_list(d_list(move_2list(test_list, rate, 3), test_list_back), item_list), exit_list), refs=True)
